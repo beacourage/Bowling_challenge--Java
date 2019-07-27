eb27490f1990fc868299fa8ab6a020c14422aec2
beatricecourage <bea_courage@hotmail.co.uk>
2019-07-27 16:42:38 +0100

    First frame strike/spare test passing

--- Tests/com/company/GameTest.java
+++ Tests/com/company/GameTest.java
@@ -16,11 +16,23 @@ public class GameTest {
     }
 
 
-    @DisplayName("First frame correctly calculates score ")
+    @DisplayName("Score calculated from firstFrame")
     @Test
     public void firstFrame() {
         AllFrames allframes = new AllFrames(1,4,4,5,6,4,5,5,10,0,0,1,7,3,6,4,10,0,8,8);
         assertEquals(5, game.getScore());
+    }
+
+    @Test
+    public void firstFrameStrike() {
+        AllFrames allframes = new AllFrames(2,10,4,5,6,4,5,5,10,0,0,1,7,3,6,4,10,0,8,8);
+        assertEquals(21, game.getScore());
+    }
+
+    @Test
+    public void firstFramesSpare() {
+        AllFrames allframes = new AllFrames(3,7,4,5,6,4,5,5,10,0,0,1,7,3,6,4,10,0,8,8);
+        assertEquals(14, game.getScore());
     }
 
 
--- src/main/java/com/company/Game.java
+++ src/main/java/com/company/Game.java
@@ -10,16 +10,14 @@ public class Game {
 
 
     public int getScore() {
-
         if ((AllFrames.getFrame1_1() == 10) || (AllFrames.getFrame1_2() == 10)) {
-            score += 10 + AllFrames.getFrame2_1() + AllFrames.getFrame2_2();
+            score += AllFrames.getFrame1_1() + AllFrames.getFrame1_2() + AllFrames.getFrame2_1() + AllFrames.getFrame2_2();
         } else if (AllFrames.getFrame1_1() + AllFrames.getFrame1_2() == 10) {
             score += 10 + AllFrames.getFrame2_1();
         } else {
             score += AllFrames.getFrame1_1() + AllFrames.getFrame1_2();
         }
     return score;
-
     }
 
 
