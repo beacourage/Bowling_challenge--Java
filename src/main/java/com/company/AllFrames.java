c5ea42592a5c517b91b556a2c1546ba174237e7e
beatricecourage <bea_courage@hotmail.co.uk>
2019-07-27 14:28:32 +0100

    Changing tack

--- src/main/java/com/company/AllFrames.java
+++ src/main/java/com/company/AllFrames.java
@@ -2,33 +2,128 @@ package com.company;
 
 public class AllFrames {
 
-    public void firstframe(int roll1, int roll2) {
+   private int frame1_1;
+   private int frame1_2;
+   private int frame2_1;
+   private int frame2_2;
+   private int frame3_1;
+   private int frame3_2;
+   private int frame4_1;
+   private int frame4_2;
+   private int frame5_1;
+   private int frame5_2;
+   private int frame6_1;
+   private int frame6_2;
+   private int frame7_1;
+   private int frame7_2;
+   private int frame8_1;
+   private int frame8_2;
+   private int frame9_1;
+   private int frame9_2;
+   private int frame10_1;
+   private int frame10_2;
 
+
+    public AllFrames(int frame1_1, int frame1_2, int frame2_1, int frame2_2, int frame3_1, int frame3_2, int frame4_1, int frame4_2, int frame5_1, int frame5_2, int frame6_1, int frame6_2, int frame7_1, int frame7_2, int frame8_1, int frame8_2, int frame9_1, int frame9_2, int frame10_1, int frame10_2) {
+        this.frame1_1 = frame1_1;
+        this.frame1_2 = frame1_2;
+        this.frame2_1 = frame2_1;
+        this.frame2_2 = frame2_2;
+        this.frame3_1 = frame3_1;
+        this.frame3_2 = frame3_2;
+        this.frame4_1 = frame4_1;
+        this.frame4_2 = frame4_2;
+        this.frame5_1 = frame5_1;
+        this.frame5_2 = frame5_2;
+        this.frame6_1 = frame6_1;
+        this.frame6_2 = frame6_2;
+        this.frame7_1 = frame7_1;
+        this.frame7_2 = frame7_2;
+        this.frame8_1 = frame8_1;
+        this.frame8_2 = frame8_2;
+        this.frame9_1 = frame9_1;
+        this.frame9_2 = frame9_2;
+        this.frame10_1 = frame10_1;
+        this.frame10_2 = frame10_2;
     }
 
+    public int getFrame1_1() {
+        return frame1_1;
+    }
 
+    public int getFrame1_2() {
+        return frame1_2;
+    }
 
+    public int getFrame2_1() {
+        return frame2_1;
+    }
 
+    public int getFrame2_2() {
+        return frame2_2;
+    }
 
+    public int getFrame3_1() {
+        return frame3_1;
+    }
 
+    public int getFrame3_2() {
+        return frame3_2;
+    }
 
+    public int getFrame4_1() {
+        return frame4_1;
+    }
 
+    public int getFrame4_2() {
+        return frame4_2;
+    }
 
+    public int getFrame5_1() {
+        return frame5_1;
+    }
 
+    public int getFrame5_2() {
+        return frame5_2;
+    }
 
+    public int getFrame6_1() {
+        return frame6_1;
+    }
 
+    public int getFrame6_2() {
+        return frame6_2;
+    }
 
+    public int getFrame7_1() {
+        return frame7_1;
+    }
 
+    public int getFrame7_2() {
+        return frame7_2;
+    }
 
+    public int getFrame8_1() {
+        return frame8_1;
+    }
 
+    public int getFrame8_2() {
+        return frame8_2;
+    }
 
+    public int getFrame9_1() {
+        return frame9_1;
+    }
 
+    public int getFrame9_2() {
+        return frame9_2;
+    }
 
+    public int getFrame10_1() {
+        return frame10_1;
+    }
 
-
-
-
-
-
-
+    public int getFrame10_2() {
+        return frame10_2;
+    }
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,28 +5,31 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-
-
         FullGame game = new FullGame();
-
-        game.all_frames(2,3);
-
-
-
+        game.all_frames(2, 3);
         System.out.println(game.getFrames());
-
-
-
         System.out.println(game.getFrames().size());
-
-
         System.out.println(game.total_game_score());
 
 
-        if (game.getFrames().contains(6)) {
-            System.out.println("contains 6");
-        } else
-            System.out.println("doesn't contain 6");
+//        if (game.getFrames().contains(6)) {
+
+
+
+
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
